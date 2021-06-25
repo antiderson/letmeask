0d6af410a22f26eb52939c8e774be846340084a7
Anderson Daniel Fonseca Aranda <anderson.adfa@pmfi.pr.gov.br>
2021-06-25 13:27:56 -0300

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+# aula 1
+criação do projeto, explicação basica sobre alguns componentes JSX entre outros;
+mapeamento da nossa aplicação a ser feita durante a semana.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import  { Button } from './components/Button'
+import { Home } from './pages/Home'
+
+export default App;
+
+function App() {
+  return (
+    <Home />
+  );
+}
